1c4c789c2322b8fca3300f1d88e0298a837dd6fc
Sudheer Bulusu <bds15@iitbbs.ac.in>
2020-12-13 13:08:58 +0530

    added candidatecontroller

---
+++ app/Http/Controllers/CandidateController.php
@@ -0,0 +1,31 @@
+<?php
+namespace TCG\Voyager\Http\Controllers;
+
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use TCG\Voyager\Facades\Voyager;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+
+class CandidateController extends Controller
+{
+    public function placement (Request $request){
+        $id = $request->route('id');        ;
+        $company = \DB::table('placements')->where('id', $id)->value('company_name');
+        $data = \DB::table('fourthyear')->where($company, '1')->get();
+
+        return Voyager::view('voyager::bread.list',compact('data', 'company') );
+ 
+    }
+
+    public function internship (Request $request){
+        $id = $request->route('id');        ;
+        $company = \DB::table('internships')->where('id', $id)->value('company_name');
+        $data = \DB::table('thirdyear')->where($company, '1')->get();
+
+        return Voyager::view('voyager::bread.list',compact('data', 'company') );
+ 
+    }
+
+}
